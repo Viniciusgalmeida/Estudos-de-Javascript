bd36d61c3f14ae74d4aa93d10fe69b32f0fe29b1
Viniciusgalmeida <viniciusgalmeida@gmail.com>
2022-12-21 15:05:39 -0300

    Exercício 20

---
+++ 20-Shortest-Word.js
@@ -0,0 +1,17 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s){
+
+    let words = s.split(' ');
+    let shortest = words[0].length;
+    for (let i = 1; i < words.length; i++) {
+        if (words[i].length < shortest) {
+            shortest = words[i].length
+        }
+    }
+    return shortest
+}
