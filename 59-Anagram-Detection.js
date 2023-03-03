ef83b85375c59976d74fa4c347e67de3d1befccf
Viniciusgalmeida <viniciusgalmeida@gmail.com>
2023-03-03 10:38:12 -0300

    Exercício 59

---
+++ 59-Anagram-Detection.js
@@ -0,0 +1,17 @@
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+let isAnagram = function (test, original) {
+  const testArray = JSON.stringify(test.toLowerCase().split("").sort());
+  const originalArray = JSON.stringify(original.toLowerCase().split("").sort());
+
+  return testArray == originalArray ? true : false;
+};
